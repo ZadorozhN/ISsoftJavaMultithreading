80497e6c57d908fe70d7c6178821649b06dc830c
zadorozhn <zadorozhny.nik@gmail.com>
2020-12-01 01:15:52 +0300

    refactoring has been done

--- OfficeBuilding/src/main/java/org/zadorozhn/Runner.java
+++ OfficeBuilding/src/main/java/org/zadorozhn/Runner.java
@@ -11,25 +11,22 @@ public class Runner {
     public static void main(String[] args){
         Building building = Building.of(10)
                 .setController(Controller.getEmpty())
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100));
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100))
-//                .addElevator(Elevator.of(5000,0,100));
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100))
+                .addElevator(Elevator.of(500,0,100));
 
-        //HumanGenerator humanGenerator = HumanGenerator.of(building, 50, 200, 100);
-        UserInterface userInterface = UserInterface.of(building, 500);
+        HumanGenerator humanGenerator = HumanGenerator.of(building, 50, 200, 100);
+        UserInterface userInterface = UserInterface.of(building, 600);
 
-        building.addHuman(Human.of(100, 2, building.getFloor(5)));
-        building.addHuman(Human.of(100, 1, building.getFloor(4)));
-
-        //humanGenerator.start();
+        humanGenerator.start();
         userInterface.start();
         building.start();
     }
