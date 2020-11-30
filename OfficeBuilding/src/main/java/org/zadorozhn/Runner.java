7600d313197b8b6343c3665ad72fad598d74c53c
zadorozhn <zadorozhny.nik@gmail.com>
2020-12-01 01:06:03 +0300

    refactoring has been done

--- OfficeBuilding/src/main/java/org/zadorozhn/Runner.java
+++ OfficeBuilding/src/main/java/org/zadorozhn/Runner.java
@@ -3,28 +3,33 @@ package org.zadorozhn;
 import org.zadorozhn.building.Building;
 import org.zadorozhn.building.Controller;
 import org.zadorozhn.building.Elevator;
+import org.zadorozhn.human.Human;
 import org.zadorozhn.util.HumanGenerator;
 import org.zadorozhn.util.UserInterface;
 
 public class Runner {
     public static void main(String[] args){
-        Building building = Building.of(2)
+        Building building = Building.of(10)
                 .setController(Controller.getEmpty())
                 .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
-                .addElevator(Elevator.of(5000,0,100))
                 .addElevator(Elevator.of(5000,0,100));
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100))
+//                .addElevator(Elevator.of(5000,0,100));
 
-        HumanGenerator humanGenerator = HumanGenerator.of(building, 50, 200, 100);
+        //HumanGenerator humanGenerator = HumanGenerator.of(building, 50, 200, 100);
         UserInterface userInterface = UserInterface.of(building, 500);
 
-        humanGenerator.start();
+        building.addHuman(Human.of(100, 2, building.getFloor(5)));
+        building.addHuman(Human.of(100, 1, building.getFloor(4)));
+
+        //humanGenerator.start();
         userInterface.start();
         building.start();
     }
--- OfficeBuilding/src/main/java/org/zadorozhn/util/interrupt/Interruptible.java
+++ OfficeBuilding/src/main/java/org/zadorozhn/util/interrupt/Interruptible.java
@@ -1,7 +1,7 @@
 package org.zadorozhn.util.interrupt;
 
 public interface Interruptible {
-    int DEFAULT_OPERATION_TIME = 100;
+    int DEFAULT_OPERATION_TIME = 1100;
     int MAX_SPEED = 1000;
     int MIN_SPEED = 100;
 
